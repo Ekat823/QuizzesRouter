f02932b7e342c1f7ce23a3b7abd816c11acb340f
Ekat <ekata823@gmail.com>
2023-03-05 01:06:59 +0300

    changes in home and navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,71 +7,71 @@ import Image from 'react-bootstrap/Image'
 
 const NavBar = () => {
   return (
-    <Container fluid>
-      <Navbar className="justify-content-center" bg="dark" style={{ padding: "3px" }} >
 
-        <Navbar.Brand ></Navbar.Brand>
+    <Navbar className="justify-content-center" bg="secondary" variant="dark" style={{ padding: "3px" }} >
 
-        <Nav>
-          <NavLink to="/">
-            <Image fluid
-              src={require('./images/home.png')}
-              style={{ padding: "20%" }}
-              alt={'Home_icon'}
-            />
-          </NavLink>
+      <Navbar.Brand ></Navbar.Brand>
 
-          <NavLink to="/user">
-            <Image fluid
-              src={require('./images/user.png')}
-              style={{ padding: "20%" }}
-              alt={'User_icon'}
-            />
-          </NavLink>
+      <Nav>
+        <NavLink to="/">
+          <Image fluid
+            src={require('./images/home.png')}
+            style={{ padding: "10% 30% 10% 30%" }}
+            alt={'Home_icon'}
+          />
+        </NavLink>
 
-          <NavLink to="/group">
-            <Image fluid
-              src={require('./images/group.png')}
-              style={{ padding: "20%" }}
-              alt={'Group_icon'}
-            />
-          </NavLink>
+        <NavLink to="/user">
+          <Image fluid
+            src={require('./images/user.png')}
+            style={{ padding: "10% 30% 10% 30%" }}
+            alt={'User_icon'}
+          />
+        </NavLink>
 
-          <NavLink to="/create_quiz">
-            <Image fluid
-              src={require('./images/create.png')}
-              style={{ padding: "20%" }}
-              alt={'Create_icon'}
-            />
-          </NavLink>
+        <NavLink to="/group">
+          <Image fluid
+            src={require('./images/group.png')}
+            style={{ padding: "10% 30% 10% 30%" }}
+            alt={'Group_icon'}
+          />
+        </NavLink>
 
-          <NavLink to="/quiz_library">
-            <Image fluid
-              src={require('./images/storage.png')}
-              style={{ padding: "20%" }}
-              alt={'Storage_icon'}
-            />
-          </NavLink>
+        <NavLink to="/create_quiz">
+          <Image fluid
+            src={require('./images/create.png')}
+            style={{ padding: "10% 30% 10% 30%" }}
+            alt={'Create_icon'}
+          />
+        </NavLink>
 
-          <NavLink to="/help">
-            <Image fluid
-              src={require('./images/question.png')}
-              style={{ padding: "20%" }}
-              alt={'Question_icon'}
-            />
-          </NavLink>
+        <NavLink to="/quiz_library">
+          <Image fluid
+            src={require('./images/storage.png')}
+            style={{ padding: "10% 30% 10% 30%" }}
+            alt={'Storage_icon'}
+          />
+        </NavLink>
 
-          <NavLink to="/search">
-            <Image fluid
-              src={require('./images/search.png')}
-              style={{ padding: "20%" }}
-              alt={'Search_icon'}
-            />
-          </NavLink>
-        </Nav>
+        <NavLink to="/help">
+          <Image fluid
+            src={require('./images/question.png')}
+            style={{ padding: "10% 30% 10% 30%" }}
+            alt={'Question_icon'}
+          />
+        </NavLink>
 
-      </Navbar>
-    </Container>
+        <NavLink to="/search">
+          <Image fluid
+            src={require('./images/search.png')}
+            style={{ padding: "10% 30% 10% 30%" }}
+            alt={'Search_icon'}
+          />
+        </NavLink>
+      </Nav>
+
+    </Navbar>
+
   )
 }
 
